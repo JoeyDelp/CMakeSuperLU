cddb3e33689ad2afe73d59c3ab97007f9d8f2f1e
JoeyDelp <joeydelp@gmail.com>
2021-08-16 12:30:58 +0200

    Fixed CMakeSuperLU

--- cmake/setup_superlu.cmake
+++ cmake/setup_superlu.cmake
@@ -2,25 +2,25 @@ cmake_minimum_required(VERSION 3.11 FATAL_ERROR)
 
 include(GNUInstallDirs)
 
+# Install target helper
 function(superlu_target name sources)
   string(TOLOWER ${name} lcname)
 
   add_library(${lcname} ${sources})
-  add_library(superlu::${lcname} ALIAS ${lcname})
 
   install(TARGETS ${lcname}
-          EXPORT superlu-${lcname}-targets
+          EXPORT ${lcname}-targets
           LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
           ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
           RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
           INCLUDES DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 
-  install(EXPORT superlu-${lcname}-targets
-          FILE CMakeSuperLU${name}Config.cmake
+  install(EXPORT ${lcname}-targets
+          FILE CMakeSuperLUConfig.cmake
           NAMESPACE superlu::
-          DESTINATION share/CMakeSuperLU${name}/)
+          DESTINATION share/CMakeSuperLU/)
 
-  set_target_properties(${lcname} PROPERTIES OUTPUT_NAME superlu_${lcname})
+  set_target_properties(${lcname} PROPERTIES OUTPUT_NAME ${lcname})
 
   # Add include directories to target
   target_include_directories(
